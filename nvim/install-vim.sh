49daa7260c11400709d93b652a3dbcc40a1eec7f
monk3yd <jegajardog@gmail.com>
2022-06-22 17:17:31 -0400

    LSP configs ready

--- nvim/install-vim.sh
+++ nvim/install-vim.sh
@@ -11,4 +11,19 @@ for f in `find . -regex ".*\.vim$\|.*\.lua$"`; do
     ln -s ~/.dotfiles/nvim/${f} ~/.config/nvim/${f}
 done
 
+# cp ~/.dotfiles/nvim/.vimrc ~/.dotfiles/nvim/init.vim
+
+# --- Copy to NVIM directory ---
+
+# .vimrc (Vim/Nvim compatibility)
+rm -rf ~/.vimrc
 ln -s ~/.dotfiles/nvim/.vimrc ~/.vimrc
+
+# init.vim
+# rm -rf ~/.config/nvim/init.vim
+# ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+
+# init.lua
+rm -rf ~/.config/nvim/lua/init.lua
+ln -s ~/.dotfiles/nvim/lua/init.lua ~/.config/nvim/lua/init.lua
+
--- nvim/lua/lsp.lua
+++
@@ -1,9 +0,0 @@
-local lspconfig = require('lspconfig')
-
-lspconfig.pyright.setup {}
-lspconfig.sumneko_lua.setup {}
-lspconfig.bashls.setup {}
-lspconfig.vimls.setup {}
-lspconfig.dockerls.setup {}
--- lspconfig.sumneko_lua.setup {}
-
