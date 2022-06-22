c13370d10fb4e1ffed47e22c63bb25c82012e7ae
monk3yd <jegajardog@gmail.com>
2022-06-22 06:06:44 -0400

    First commit. nvim config.

---
+++ nvim/lua/lsp.lua
@@ -0,0 +1,9 @@
+local lspconfig = require('lspconfig')
+
+lspconfig.pyright.setup {}
+lspconfig.sumneko_lua.setup {}
+lspconfig.bashls.setup {}
+lspconfig.vimls.setup {}
+lspconfig.dockerls.setup {}
+-- lspconfig.sumneko_lua.setup {}
+
