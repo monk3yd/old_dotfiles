0965049c27f054297f6f92b94d832471fda008d0
monk3yd <jegajardog@gmail.com>
2022-07-27 04:42:29 -0300

    Focus on activation smart.

--- qtile/config.py
+++ qtile/config.py
@@ -553,7 +553,7 @@ floating_layout = layout.Floating(
 )
 auto_fullscreen = True
 
-focus_on_window_activation = "focus"  # or smart
+focus_on_window_activation = "smart"  # or smart
 
 wmname = "LG3D"
 
