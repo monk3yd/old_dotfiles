e55f84c02dc5ee6cafddc07f329aaa83f6688d56
monk3yd <jegajardog@gmail.com>
2022-07-29 22:47:33 -0300

    Update autostart apps, need fixing.

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -41,27 +41,21 @@ run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
 
 #starting utility applications at boot time
 run alacritty
+picom --config $HOME/.config/qtile/scripts/picom.conf &
 # run todoist &  # TODO
 # run variety &
 run nm-applet &
 run pamac-tray &
 # run xfce4-power-manager &
 numlockx on &
-blueberry-tray &
-picom --config $HOME/.config/qtile/scripts/picom.conf &
+blueman-applet &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 
 #starting user applications at boot time
 run volumeicon &
+run insync start &
 #run discord &
 #nitrogen --restore &
-#run caffeine -a &
-#run vivaldi-stable &
 #run firefox &
 #run thunar &
-#run dropbox &
-run insync start &
-#run spotify &
-#run atom &
-#run telegram-desktop &
